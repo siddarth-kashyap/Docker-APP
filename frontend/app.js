b5150152298c22686660230ff05d462e2608865c
Siddarth Kashyap <50227008+siddarth-kashyap@users.noreply.github.com>
2026-05-01 18:28:53 +0530

    Update app.js
    Updated for CI/CD pipeline

--- frontend/app.js
+++ frontend/app.js
@@ -14,14 +14,14 @@ app.get('/', (req, res) => {
 app.post('/submit', async (req, res) => {
     const userData = req.body;
 
-    const backendUrl = process.env.BACKEND_URL || 'http://flask-backend:5000/api/process';
+    const backendUrl = process.env.BACKEND_URL || 'http://127.0.0.1:5000/api/process';
     try {
         // 'backend' will be the name of the Docker service later
         const response = await axios.post(backendUrl, userData);
         res.send(`Backend says: ${JSON.stringify(response.data)}`);
     } catch (error) {
-        console.error(error);
-        res.status(500).send("Error connecting to backend");
+        console.error("Connection Error:", error.message);
+        res.status(500).send("Error connecting to backend" + backendUrl);
     }
 });
 
